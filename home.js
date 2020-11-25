7944080b72a3c977fc95738d8c77e1399a19d3c7
arbabali1441 <arbabali1441@gmail.com>
2020-11-25 13:35:37 +0500

    Numbers in Javascript @ 15:49

---
+++ home.js
@@ -0,0 +1,15 @@
+console.log('Hello');
+
+// alert('Hey this is Arbab');
+
+// How to write a comment inline
+
+// Variables 
+var b = 'super';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+var age = prompt('What is you Name?');
+document.getElementById('someText').innerHTML = age;
