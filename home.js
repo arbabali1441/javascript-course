ac4c4d7a9b63c188058b5f6d0dbb215391b0e7c2
arbabali1441 <arbabali1441@gmail.com>
2021-01-19 04:58:35 +0500

    Add javascript intro hello javascript demo

--- home.js
+++ home.js
@@ -13,3 +13,8 @@ console.log(someNumber);
 
 var age = prompt('What is you Name?');
 document.getElementById('someText').innerHTML = age;
+
+// Numbers in Javascript
+var num1 = 10;
+num1 = num1 + 1;
+console.log(num1);
